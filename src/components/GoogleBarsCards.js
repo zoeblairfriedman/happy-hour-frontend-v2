dc69e1ce7eef269594aa10560a5ef7118512354f
Zoe Friedman <zoeblair.friedman@gmail.com>
2021-07-30 10:40:39 -0400

    moves edit and input forms to modals

--- src/components/GoogleBarsCards.js
+++ src/components/GoogleBarsCards.js
@@ -1,17 +1,31 @@
 import React from 'react';
 import { useSelector } from 'react-redux'
-import {CardColumns, Card, Button} from 'react-bootstrap'
+import  {useState } from "react";
+import {CardColumns, Card, Button, Modal} from 'react-bootstrap'
+import BarInput from './BarInput'
+import BarEdit from './BarEdit'
 
 function GoogleBarsCards(){
 
 const googleBars = useSelector(state => state.googleBars)
 const bars = useSelector(state => state.bars)
 
+const [show, setShow] = useState(false);
+const handleClose = () => setShow(false);
+const [selected, setSelected] = useState()
+
+const handleShow = function(bar) {
+  setShow(true)
+  setSelected(bar)
+}
+
+
 function returnBar(bar){
   return bars.find(b => b.placeId === bar.place_id)
 }
 
     return (
+      <>
         <CardColumns>
              {googleBars.map(b => 
             <Card>
@@ -21,15 +35,34 @@ function returnBar(bar){
               <Card.Text>
               {b.vicinity}
               </Card.Text>
-              {!returnBar(b) ? <Button variant="primary">
+              {!returnBar(b) ? <Button variant="primary" onClick={() => handleShow(b)}>
                 Add Happy Hour Information!
-                </Button> : <Button variant="secondary">
+                </Button> : <Button variant="secondary" onClick={() => handleShow(b)}>
                   Verify or Edit Happy Hour Deets!
                   </Button>}   
             </Card.Body>
           </Card>
             )}
         </CardColumns>
+
+        <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Modal heading</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+        {!!returnBar(selected) ? <BarEdit bar={returnBar(selected)}/> : <BarInput bar={selected}/>} 
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+          <Button variant="primary" onClick={handleClose}>
+            Save Changes
+          </Button>
+        </Modal.Footer>
+      </Modal>
+
+      </>
     )
 }
 
